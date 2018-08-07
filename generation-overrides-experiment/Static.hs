644ec685f7bb12aa9cdef4ad9cd72b9f1d4fb880
Nathan Collins <conathan@galois.com>
2018-08-07 15:47:31 -0700

    Checkpoint: failed approach to overrides for parameterized types
    Turns out that the type of field is not sufficient to determine how it
    gets overridden. Rather, we need to use something like the full
    context of the field, including it's containing type. I'll work that
    next, but am recording the deadend here, since most of the changes
    here will be preserved.

--- generation-overrides-experiment/Static.hs
+++ generation-overrides-experiment/Static.hs
@@ -15,6 +15,8 @@ import Data.Typeable ( Typeable, (:~:)(Refl), eqT )
 import GHC.TypeLits ( KnownSymbol, Symbol )
 import Text.Printf
 
+import qualified System.Random as R
+
 ----------------------------------------------------------------
 -- * Notes
 --
@@ -227,20 +229,53 @@ data Field (t :: *) (f :: Symbol) where
           , Typeable ft )
        => Field t f
 
+infixr 5 :.
 -- | A path into a data type and a generator for the type at the end
 -- of the path.
 data Override (t :: *) where
   (:=) :: FieldTy t f ~ ft
        => Field t f
-       -> ([Override ft] -> PadsGen ft)
+       -> ([Override ft] -> PadsGenTy ft)
        -> Override t
   (:.) :: FieldTy t f ~ ft
        => Field t f
        -> Override ft
        -> Override t
-infixr 5 :.
 
+-- | The field type of a constructor field in a data type.
+--
+-- The @FieldTy <type> "<constructor>.<field>"@ is the type of field
+-- @<field>@ of constructor @<constructor>@ in type @<type>@.
+--
+-- Then constructor @<constructor>@ is not a record constructor, the
+-- fields are referred to using zero based indices. See the @E a b@
+-- example.
 type family FieldTy (t :: *) (f :: Symbol) :: *
+
+-- | The PADS library provides
+
+-- | The PADS generator type for a field.
+--
+-- The generator type
+--
+-- > PadsGenTy [pads| T a1 ... am (i1 :: t1, ..., in :: tn) = ... |]
+--
+-- is
+--
+-- > PadsGen a1 -> ... -> PadsGen am -> (t1, ..., tn) -> PadsGen (T a1 ... am)
+--
+-- when @n > 0@ and
+--
+-- > PadsGen a1 -> ... -> PadsGen am -> PadsGen (T a1 ... am)
+--
+-- otherwise.
+type family PadsGenTy (t :: *) :: *
+
+-- | The PADS library provides the @PadsGenTy@ instances for the built
+-- in types.
+type instance PadsGenTy String = PadsGen String
+type instance PadsGenTy Int = PadsGen Int
+type instance PadsGenTy Bytes = Int -> PadsGen Bytes
 
 -- | Lookup the override for the given field, if any, and use the
 -- default generator otherwise.
@@ -257,14 +292,14 @@ type family FieldTy (t :: *) (f :: Symbol) :: *
 -- cases we just pass the list of relevant overrides down the call
 -- tree; in case (3) the override list just happens to be empty.
 override :: forall t f ft. FieldTy t f ~ ft =>
-  Field t f -> ([Override ft] -> PadsGen ft) -> [Override t] -> PadsGen ft
+  Field t f -> ([Override ft] -> PadsGenTy ft) -> [Override t] -> PadsGenTy ft
 override (:@) defaultGen ovs = gen filteredOvs
   where
     gen = case findExactOv ovs of
       Just ovGen -> ovGen
       Nothing -> defaultGen
 
-    findExactOv :: [Override t] -> Maybe ([Override ft] -> PadsGen ft)
+    findExactOv :: [Override t] -> Maybe ([Override ft] -> PadsGenTy ft)
     findExactOv [] = Nothing
     findExactOv (ov : ovs') =
       case ov of
@@ -289,6 +324,21 @@ override (:@) defaultGen ovs = gen filteredOvs
 defaultOvs :: [Override t]
 defaultOvs = []
 
+-- | Choose a computation from a (non-empty!) list.
+--
+-- Ides is to use this to choose a constructor generation from a sum
+-- type.
+choose :: [PadsGen a] -> PadsGen a
+choose choices = do
+  index <- R.getStdRandom $ (R.randomR (0,len-1))
+  choices !! index
+  where
+    len = case length choices of
+      0 -> error "choose: BUG: zero choices!"
+      n -> n
+
+-- ** Datatype specific built-in PADS library code
+
 genInt :: PadsGen Int
 genInt = genIntOvs defaultOvs
 
@@ -300,6 +350,15 @@ genString = genStringOvs defaultOvs
 
 genStringOvs :: [Override String] -> PadsGen String
 genStringOvs _ = return "default"
+
+-- | Built into PADS.
+data Bytes = Bytes Int
+
+genBytes :: PadsGenTy Bytes
+genBytes n = return (Bytes n)
+
+genBytesOvs :: [Override Bytes] -> PadsGenTy Bytes
+genBytesOvs _ = genBytes
 
 ----------------------------------------------------------------
 -- * Example of what PADS would generate
@@ -319,6 +378,9 @@ type instance FieldTy R1 "R1.y" = Int
 type instance FieldTy R2 "R2.r1" = R1
 type instance FieldTy R2 "R2.z" = Int
 
+type instance PadsGenTy R1 = PadsGen R1
+type instance PadsGenTy R2 = PadsGen R2
+
 genR1 :: PadsGen R1
 genR1 = genR1Ovs defaultOvs
 
@@ -336,6 +398,64 @@ genR2Ovs ovs = do
   r1 <- override ((:@) @R2 @"R2.r1") genR1Ovs ovs
   z <- override ((:@) @R2 @"R2.z") genIntOvs ovs
   return R2{..}
+
+-- ** Examples with parameters
+--
+-- > [pads|
+-- >   data E a b = L a | M a b | R b
+-- >   data A (n :: Int) = A { a_a :: Bytes n }
+-- > |]
+
+data E a b = L a | M a b | R b
+data A = A { a_a :: Bytes }
+
+type instance FieldTy (E a b) "L.0" = a
+type instance FieldTy (E a b) "M.0" = a
+type instance FieldTy (E a b) "M.1" = b
+type instance FieldTy (E a b) "R.0" = b
+type instance PadsGenTy (E a b) = PadsGen a -> PadsGen b -> PadsGen (E a b)
+
+type instance FieldTy A "A.a_a" = Bytes
+type instance PadsGenTy A = Int -> PadsGen Bytes
+
+-- Q: Should the argument generators, e.g. in @genEOvs@ below, also
+-- receive overrides?
+--
+-- A1: Yes, since they're used to override fields?
+--
+-- A2: No, since they could be used nested in which case we don't know
+-- what overrides to use with them?
+--
+-- A3: Yes, but only the wildcard overrides used to e.g. change the
+-- distribution of base types?
+--
+-- ???
+genEOvs :: forall a b. [Override (E a b)] -> PadsGenTy (E a b)
+genEOvs ovs genA genB = choose
+  -- Problem: as an argument to @genEOvs@ we have @genA :: PadsGen a@,
+  -- but when passing it to @override@ in generating a field we need
+  -- @genA :: PadsGenTy a@, and those are not the same in general. In
+  -- fact, they're not even the same shape, in that @PadsGenTy a@ will
+  -- take a variable number of arguments, depending on what @a@
+  -- actually is.
+  --
+  -- Solution: replace @PadsGenTy@ with a new type family @FieldGenTy@
+  -- that computes the generator type based on the field in the
+  -- enclosing type, not the field type itself. This will allow us to
+  -- implement "the user can override the first component of the
+  -- generation" semantics, and could perhaps even be generalized to
+  -- overriding any component of the generation, at the expense of
+  -- complicating the interface.
+  [ do a <- override ((:@) @(E a b) @"L.0") (const genA) ovs
+       return $ L a
+  , do a <- genA
+       b <- genB
+       return $ M a b
+  , do b <- genB
+       return $ R b
+  ]
+
+
 
 ----------------------------------------------------------------
 -- * Example of how user can then override generation
